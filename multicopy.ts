f80ae4b68fc46ad451149524df70baf2b14dc6ef
Jongmin Lee <jakelee@woowahan.com>
2023-09-02 22:07:28 +0900

    feat: add commitlint

--- multicopy.ts
+++ multicopy.ts
@@ -3,20 +3,26 @@ import { join } from 'path';
 
 /**
  * should migrate to `nx` but until then, this is a quick way to maintain repository
- * @useage `ts-node multicopy.ts`
+ * @usage `ts-node multicopy.ts`
  */
-const filename = '.prettierrc.js';
+const filenames = ['.prettierrc.js'];
+const excludeDirs = ['.git', '.vscode', '.reference', 'node_modules'];
 
-const workspaceRoot = join(process.cwd());
-const filePath = join(workspaceRoot, filename);
-const exclude = ['.git', '.vscode', 'node_modules'];
-readdirSync(workspaceRoot, { recursive: false }).forEach((folder) => {
-  if (typeof folder !== 'string') {
-    return;
+async function main() {
+  for (const filename of filenames) {
+    const workspaceRoot = join(process.cwd());
+    const filePath = join(workspaceRoot, filename);
+    readdirSync(workspaceRoot, { recursive: false }).forEach((folder) => {
+      if (typeof folder !== 'string') {
+        return;
+      }
+      const destDir = join(workspaceRoot, folder);
+      if (lstatSync(destDir).isDirectory() && !excludeDirs.includes(destDir)) {
+        const destPath = join(destDir, filename);
+        copyFileSync(filePath, destPath);
+      }
+    });
   }
-  const destDir = join(workspaceRoot, folder);
-  if (lstatSync(destDir).isDirectory() && !exclude.includes(destDir)) {
-    const destPath = join(destDir, filename);
-    copyFileSync(filePath, destPath);
-  }
-});
+}
+
+main();
