b4f1ce0ac5964395c1aec5da9262cfbfdcf1b629
thilllon <thilllon970@gmail.com>
2024-05-08 21:10:59 +0900

    Issue 252 (#258)
    * feat: add cloudinary
    
    * feat: add nestjs-tools
    
    * feat: add image
    
    * test: add tests

---
+++ libs/@nestjs-tools-cloudinary/src/lib/cloudinary.module.ts
@@ -0,0 +1,18 @@
+import { Module, OnModuleInit } from '@nestjs/common';
+
+import { ConfigurableModuleClass } from './cloudinary.module-definition';
+import { CloudinaryService } from './cloudinary.service';
+
+@Module({
+  providers: [CloudinaryService],
+  exports: [CloudinaryService],
+})
+export class CloudinaryModule extends ConfigurableModuleClass implements OnModuleInit {
+  constructor(private readonly service: CloudinaryService) {
+    super();
+  }
+
+  async onModuleInit() {
+    this.service.ping();
+  }
+}
